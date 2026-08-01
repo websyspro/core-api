f617907a2bd5bcd27a8190bb3b1025449aa32bb2
websyspro <cpd.emersontsa@gmail.com>
2026-07-31 21:16:06 -0300

    Update components

--- src/Applications/Global/Controllers/PostsController.php
+++ src/Applications/Global/Controllers/PostsController.php
@@ -2,13 +2,12 @@
 
 namespace Websyspro\Server\Applications\Global\Controllers;
 
-use Websyspro\Server\Applications\Global\Views\PostsView;
 use Websyspro\Server\Includes\Decorators\Server\AllowAnonymous;
 use Websyspro\Server\Includes\Decorators\Server\Authenticate;
-use Websyspro\Server\Includes\Decorators\Server\Body;
 use Websyspro\Server\Includes\Decorators\Server\Controller;
-use Websyspro\Server\Includes\Decorators\Server\Get;
 use Websyspro\Server\Includes\Interfaces\QueryViewModel;
+use Websyspro\Server\Includes\Decorators\Server\Body;
+use Websyspro\Server\Includes\Decorators\Server\Get;
 
 #[Authenticate()]
 #[Controller( "posts" )]
